1db2e9c76a55f71ca8c3c18f675b242e7c3d791f
zakirah14 <rania.zakirah14@gmail.com>
2023-11-17 13:33:23 +0700

    first commit

---
+++ DashboardLatihan/src/components/EssentialLink.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-item
+    clickable
+    v-ripple
+    :class="$route.name == routeName ? 'activeItem' : ''"
+    @click="goToPage(routeName)"
+  >
+    
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    routeName: {
+      type: String,
+      required: true
+    }
+  },
+
+  setup() {
+
+    return {
+      linkActive: ref(''),
+    }
+  },
+  
+  methods: {
+    goToPage(routeName: string) {
+      this.linkActive = routeName;
+      this.$router.push({ name: routeName });
+    },
+  },
+});
+</script>
